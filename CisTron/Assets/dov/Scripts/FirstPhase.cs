21c7267f794a1609658eb53184cdf033ec005f59
Benzakk <dovbenzak2@hotmail.com>
2020-06-18 17:42:53 +0200

    Fin Mise en place Première phase

--- CisTron/Assets/dov/Scripts/FirstPhase.cs
+++ CisTron/Assets/dov/Scripts/FirstPhase.cs
@@ -5,60 +5,67 @@ using UnityEngine;
 public class FirstPhase : MonoBehaviour
 {
     public GameObject[] fruitTab;
-    public bool isActive;
+    private bool isActive;
     private GameObject theObject;
     public float waitInstance = 0;
     public int score = 0;
     public int count = 0;
     List<Collider2D> listCol = new List<Collider2D>();
     List<Vector3> listPos = new List<Vector3>();
+    private bool startGame;
 
     private void Start()
     {
         isActive = true;
+        startGame = true;
     }
 
     private void Update()
     {
-        if (isActive)
-        {
-            StartCoroutine(FruitInstance());
-        }
-        
+        StartCoroutine(TimeFirstPhase());
 
-        if (Input.touchCount > 0)
+        if (startGame)
         {
-            Touch touch = Input.GetTouch(0);
-            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-
-            if (touch.phase == TouchPhase.Began)
+            if (isActive)
             {
-                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
-                for (int i = 0; i < listCol.Count; i++)
+                StartCoroutine(FruitInstance());
+            }
+
+
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+                if (touch.phase == TouchPhase.Began)
                 {
-                    if (listCol[i] == touchedCollider)
+                    Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                    for (int i = 0; i < listCol.Count; i++)
                     {
-                        Destroy(listCol[i].gameObject);
-                        listCol.RemoveAt(i);
-                        listPos.RemoveAt(i);
-                        if (listCol[i].tag == "CitronVert")
+                        if (listCol[i] == touchedCollider)
                         {
-                            if (count >= 5)
-                                WinStrick();
-                            
-                            else
-                                score += 1;
-                        }
-                        else
-                        {
-                            if (count >= 5)
-                                LoseStrick();
+                            if (listCol[i].tag == "CitronVert")
+                            {
+                                if (count >= 5)
+                                    WinStrick();
 
+                                else
+                                    score += 1;
+                            }
                             else
-                                score -= 1;
-                        }
+                            {
+                                if (count >= 5)
+                                    LoseStrick();
 
-                        count += 1;
+                                else
+                                    score -= 1;
+                            }
+                            count += 1;
+
+                            Destroy(listCol[i].gameObject);
+                            listCol.RemoveAt(i);
+                            listPos.RemoveAt(i);
+                        }
                     }
                 }
             }
@@ -78,11 +85,16 @@ public class FirstPhase : MonoBehaviour
     IEnumerator FruitInstance()
     {
         isActive = false;
-        theObject = fruitTab[Random.Range(0, 5)];
+        theObject = fruitTab[Random.Range(0, 7)];
         Vector3 pos = new Vector3(Random.Range(-3, 3), Random.Range(-2, 2), 0);
         
         for (int i = 0; i < listPos.Count; i++)
         {
+            if (listCol[i] == null)
+            {
+                listCol.RemoveAt(i);
+                listPos.RemoveAt(i);
+            }
             if (pos == listPos[i])
             {
                 isActive = true;
@@ -94,5 +106,24 @@ public class FirstPhase : MonoBehaviour
         listPos.Add(pos);
         yield return new WaitForSeconds(waitInstance);
         isActive = true;
+    }
+
+
+    IEnumerator TimeFirstPhase()
+    {
+        yield return new WaitForSeconds(25);
+        startGame = false;
+    }
+
+    public void DeleteList(Collider2D collider)
+    {
+        for (int i = 0; i < listCol.Count; i++)
+        {
+            if (listCol[i] == collider)
+            {
+                listCol.RemoveAt(i);
+                listPos.RemoveAt(i);
+            }
+        }
     }
 }
